7f003891030ec6785f1a40ed8bd6feeb4f1e7316
Daniel Lopez <flatfootdesign@gmail.com>
2020-10-07 09:33:24 -0500

    react app as of chapter 3

--- src/components/header.js
+++ src/components/header.js
@@ -3,40 +3,15 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+    <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+    <Link to="/about">Page 2</Link>
+    <Link to="/work">Page 2</Link>
+    <Link to="/downloads">Page 2</Link>
+    <Link to="/buy"><button>Buy</button></Link>
     </div>
-  </header>
+  </div>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import '../components/Header.css'
+import Header from '../components/header.js'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Welcome to this website</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Next page</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+// To wrap text in VSCode, Alt+Z
+<div>
+    <Header />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <a href="">Watch the video</a>
+      </div>
+    </div>
+
+    <h1>Daniel Lopez</h1>
+    <p>Senior Product Designer at Eight Bit Studios in Chicago.</p>
+    <p>LinkedIn. Dribble. About. Work.</p>
+    <Link to="/page-2/">Next page</Link> 
+  </div>
 )
 
 export default IndexPage
