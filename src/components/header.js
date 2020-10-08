5be909eb47e49cd7911150448adc8162012db805
Daniel Lopez <flatfootdesign@gmail.com>
2020-10-08 16:19:48 -0500

    updates through the svg animation chapter

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,16 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import './Header.css'
+
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-    <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-    <Link to="/about">Page 2</Link>
-    <Link to="/work">Page 2</Link>
-    <Link to="/downloads">Page 2</Link>
+    <Link to="/"><img src={require
+      ('../images/logo-designcode.svg')} width="30" /></Link>
+    <Link to="/about">About</Link>
+    <Link to="/work">Work</Link>
+    <Link to="/downloads">Downloads</Link>
     <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
